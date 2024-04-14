07f756d5a4fddd7d40486e83faa8ccd202a6d352
Nguyen Pham Thanh Phong <thanhphong17160@gmail.com>
2024-04-15 05:00:31 +0700

    change userUpdateRequest

--- CookingRecipeApi/RequestsResponses/UserRequests/UserUpdateRequest.cs
+++ CookingRecipeApi/RequestsResponses/UserRequests/UserUpdateRequest.cs
@@ -4,18 +4,19 @@ namespace CookingRecipeApi.RequestsResponses.UserRequests
 {
     public class UserUpdateRequest
     {
-#pragma warning disable CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
+//#pragma warning disable CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
         [Required]
         [StringLength(50, MinimumLength = 1)]
         public string fullName { get; set; }
-        public string avatarUrl { get; set; }
+        public string? avatarUrl { get; set; }
         public IFormFile? avatarImg { get; set; }
-        public bool isVegan { get; set; }
-        public string bio { get; set; }
+        public bool? isVegan { get; set; }
+        public string? bio { get; set; }
         public List<string> categories { get; set; }
         public int hungryHeads { get; set; }
         public UserUpdateRequest()
         {
+            fullName = string.Empty;
             categories = new List<string>();
             avatarUrl = string.Empty;
             isVegan = false;
